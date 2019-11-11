2c9e70e455714da23f645f6c3b817cb2e8ed7b55
Geovani Granieri <geovani.hilario@outlook.com>
2019-11-11 14:26:30 -0300

    Responsividade e Mais Conceitos

--- funcoesAuxiliares.js
+++ funcoesAuxiliares.js
@@ -5,25 +5,10 @@ const reducer = (accumulator, currentValue) => accumulator + currentValue;
 const somatorio = array => array.map(element => element.y).reduce(reducer);
 
 
-const bubbleData = {
-    Media1k: 0,
-    Media10k: 0,
-    Media100k: 0,
-}
+const newValue = data.SortingData.bubble10k;
 
-const quickData = {
-    Media1k: 0,
-    Media10k: 0,
-    Media100k: 0,
-}
+const newType = newValue.map( element => element.y / 1e+6)
 
-bubbleData.Media1k = somatorio(data.SortingData.bubble1k) / 10 / 1000;
-bubbleData.Media10k = somatorio(data.SortingData.bubble10k) / 10 / 1000;
-bubbleData.Media100k = somatorio(data.SortingData.bubble100k) / 10 / 1000;
-
-quickData.Media1k = somatorio(data.SortingData.quicksort1k) / 10 / 1000;
-quickData.Media10k = somatorio(data.SortingData.quicksort10k) / 10 / 1000;
-quickData.Media100k = somatorio(data.SortingData.quicksort100k) / 10 / 1000;
-
-console.log(bubbleData);
-console.log(quickData);
+newValue.map( (element, index) => {
+    element.y = newType[index];
+})
