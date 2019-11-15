2a4361133cf4bd366e522a240edad8f24baad867
geovaniggr <geovani.granieri@outlook.com>
2019-11-15 11:35:57 -0300

    Adição do QuickSort x MergeSort

--- NanoToSeg.js
+++ NanoToSeg.js
@@ -1,5 +1,6 @@
 const bubbleData = require('./DataBubble');
 const quickData = require('./DataQuick');
+const mergeData = require('./DataMerge');
 
 const reducer = (acumulador, valor) => acumulador + valor;
 const somatorio = array => array.reduce(reducer);
@@ -12,6 +13,22 @@ const toMs = element => element / 1e+6;
 const bubble1 = converterUnidade(bubbleData.bubble1k, toSeg)
 const bubble10 = converterUnidade(bubbleData.bubble10k, toSeg);
 const bubble100 = converterUnidade(bubbleData.bubble100k, toSeg);
+
 const quick1 = converterUnidade(quickData.quick1K, toSeg);
 const quick10 = converterUnidade(quickData.quick10K, toSeg);
 const quick100 = converterUnidade(quickData.quick100K, toSeg);
+
+const merge1 = converterUnidade(mergeData.merge1k, toSeg);
+const merge10 = converterUnidade(mergeData.merge10k, toSeg);
+const merge100 = converterUnidade(mergeData.merge100k, toSeg);
+
+const arrayCorreto = [...mergeData.merge100k]
+
+// arrayCorreto.map( (element, index) => {
+//     element.x = element.x;
+//     element.y = merge100[index]
+// })
+
+// console.log(arrayCorreto);
+
+console.log(toSeg(209972088));
--- criarDadosOrdenar.js
+++ criarDadosOrdenar.js
@@ -1,21 +1,23 @@
 const fs = require('fs');
 const $ = require('./Quicksort');
 const bubble = require('./BubbleSort');
+const merge = require('./MergeSort');
 const process = require('process');
 
 const ordenar = (array, qtde) => {
     const time = process.hrtime();
-    //$.quickSort(array, 0, array.length -1);
-    bubble.bubbleSort(array);
+    $.quickSort(array, 0, array.length -1);
+    //bubble.bubbleSort(array);
+    //merge.mergeSort(array);
     const tempoFinal = process.hrtime(time);
     const tempoTotal = tempoFinal[0] * 1e9 + tempoFinal[1];
-    console.log(`O tempo de execução foi de ${tempoTotal} nanosegundos --- ${tempoTotal / 1e+9} segundos`)
+    console.log(`O tempo de execução foi de ${tempoTotal} nanosegundos --- ${tempoTotal / 1e+9} segundos`);
     return criarDados(`{x:${qtde}, y:${tempoTotal}},\n`, qtde)
 }
 
 function criarDados(tempo, nome) {
 
-    fs.appendFile(`Data-Quick-${nome}.txt`, tempo, function(err){
+    fs.appendFile(`Data-QuickO-${nome}.txt`, tempo, function(err){
         if(err) throw err;
     });
 }
@@ -28,15 +30,17 @@ function lerArquivo(nome, tipo) {
 
 (function main() {
 
-    for(let i = 0; i < 50; i++){
-        lerArquivo(`./content/array-${i}-1000.txt`, 1000)
-    }
-    for(let i = 0; i < 50; i++){
-        lerArquivo(`./content/array-${i}-10000.txt`, 10000)
-    }
-    for(let i = 0; i < 50; i++){
-       lerArquivo(`./content/array-${i}-100000.txt`, 100000)
-    }
+    // for(let i = 0; i < 50; i++){
+    //     lerArquivo(`./content/array-${i}-1000.txt`, 1000)
+    // }
+    // for(let i = 0; i < 50; i++){
+    //     lerArquivo(`./content/array-${i}-10000.txt`, 10000)
+    // }
+    // for(let i = 0; i < 50; i++){
+    //    lerArquivo(`./content/array-${i}-100000.txt`, 100000)
+    // }
+    lerArquivo('./content/arrayOrdenado.txt', 100000);
+
 
 
 })();
